74560986d321d217dd51d8ada9b297630a19ab11
Cole Hudson <m.cole.hudson@gmail.com>
2026-05-19 16:30:30 -0500

    Revert "Trying another docstring format to get LSPs to pick it up"
    This reverts commit d06d945031d3deedabde17783527672ff9ef1210.

--- src/polaroids/__init__.py
+++ src/polaroids/__init__.py
@@ -113,31 +113,17 @@ def generate_stubs(
             safe_col_name = safe_col_name.lower()
         if safe_col_name != col_name:
             rename_mapping[col_name] = safe_col_name
-            lines.extend(
-                [
-                    '    @property',
-                    f'    def {safe_col_name}(self) -> pl.Expr:',
-                    f'        """dtype: {dtype}, original name:{col_name}"""',
-                    '        ...',
-                ]
-            )
+            lines.append(f'    {safe_col_name}: pl.Expr')
+            lines.append(f'    """dtype: {dtype}, original name:{col_name}"""')
         else:
-            lines.extend(
-                [
-                    '    @property',
-                    f'    def {safe_col_name}(self) -> pl.Expr:',
-                    f'        """dtype: {dtype}"""',
-                    '        ...',
-                ]
-            )
+            lines.append(f'    {safe_col_name}: pl.Expr')
+            lines.append(f'    """dtype: {dtype}"""')
 
     lines.extend(
         [
             '',
             f'class {class_name}(pl.DataFrame):',
-            '    @property',
-            f'    def c(self) -> _{class_name}_cols:',
-            '        ...',
+            f'    c: _{class_name}_cols',
             f'# --- END {class_name} ---',
         ]
     )
