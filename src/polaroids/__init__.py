a3d2df9f4a2d295919c9db2ca1d891f11169fae0
Cole Hudson <m.cole.hudson@gmail.com>
2026-05-20 14:06:53 -0500

    Added `df.s` accessor for string representations of columns names

--- src/polaroids/__init__.py
+++ src/polaroids/__init__.py
@@ -71,6 +71,70 @@ class LazyColumnAccessor:
         return self._df.columns
 
 
+@pl.api.register_dataframe_namespace('s')
+class ColumnStringAccessor:
+    """Provides dot notation access to polars dataframe column names as strings."""
+
+    def __init__(self, df: pl.DataFrame) -> None:
+        """Initializes column name string accessor.
+
+        Args:
+            df (pl.DataFrame): Polars dataframe to wrap
+        """
+        self._df = df
+
+    def __getattr__(self, col_name: str) -> str:
+        """Gets column name as a string and returns it.
+
+        Args:
+            col_name (str): name of dataframe column
+
+        Returns:
+            str: name of column as a string
+        """
+        return col_name
+
+    def __dir__(self) -> list[str]:
+        """Returns list of column names for environments which need it.
+
+        Returns:
+            list[str]: list of column names in dataframe
+        """
+        return self._df.columns
+
+
+@pl.api.register_lazyframe_namespace('s')
+class LazyColumnStringAccessor:
+    """Provides dot notation access to polars lazyframe column names as strings."""
+
+    def __init__(self, df: pl.LazyFrame) -> None:
+        """Initializes column name string accessor.
+
+        Args:
+            df (pl.LazyFrame): Polars lazyframe to wrap
+        """
+        self._df = df
+
+    def __getattr__(self, col_name: str) -> str:
+        """Gets column name as a string and returns it.
+
+        Args:
+            col_name (str): name of lazyframe column
+
+        Returns:
+            str: name of column as a string
+        """
+        return col_name
+
+    def __dir__(self) -> list[str]:
+        """Returns list of column names for environments which need it.
+
+        Returns:
+            list[str]: list of column names in lazyframe
+        """
+        return self._df.columns
+
+
 def generate_stubs(
     df: Union[pl.DataFrame, pl.LazyFrame],
     class_name: str,
@@ -105,6 +169,7 @@ def generate_stubs(
     all_col_names = set()
     col_classes = []
     col_attributes = [f'class {class_name}Cols:']
+    col_str_attributes = [f'class {class_name}StrCols:']
     for col_name, dtype in schema.items():
         safe_col_name = re.sub(r'[^0-9a-zA-Z_]', '_', col_name)
         safe_col_name = re.sub(r'_+', '_', safe_col_name).strip('_')
@@ -132,21 +197,35 @@ def generate_stubs(
             doc_str = f'dtype: {dtype}'
 
         col_class_name = f'{class_name}_{safe_col_name}'
+        col_str_class_name = f'{class_name}_{safe_col_name}_Str'
 
         col_classes.extend(
-            [f'class {col_class_name}(pl.Expr):', f'    """{doc_str}"""', '    ...']
+            [
+                f'class {col_class_name}(pl.Expr):',
+                f'    """{doc_str}"""',
+                '    ...',
+                f'class {col_str_class_name}(str):',
+                f'    """{doc_str}"""',
+                '    ...',
+            ]
         )
         col_attributes.append(f'    {safe_col_name}: {col_class_name}')
+        col_str_attributes.append(f'    {safe_col_name}: {col_str_class_name}')
 
     lines.extend(col_classes)
     lines.append('')
     lines.extend(col_attributes)
+    lines.append('')
+    lines.extend(col_str_attributes)
     lines.extend(
         [
             '',
             f'class {class_name}(pl.DataFrame):',
             '    @property',
             f'    def c(self) -> {class_name}Cols:',
+            '        ...',
+            '    @property',
+            f'    def s(self) -> {class_name}StrCols:',
             '        ...',
             f'# --- END {class_name} ---',
             '',
